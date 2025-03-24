57cc23c8719877a43b513cfeb495e74ff6bb9d5b
rmfust20 <robertmfusting@gmail.com>
2025-03-24 12:01:19 -0400

    frontend init

---
+++ Frontend/TechIOSApp/TechIOSApp/TechIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechIOSAppApp.swift
+//  TechIOSApp
+//
+//  Created by Robert Fusting on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct TechIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
