5e0eb8f3059ed73ffecb5a11141c0e097bce81e6
rmfust20 <robertmfusting@gmail.com>
2025-03-24 11:57:02 -0400

    frontend init

---
+++ Frontend/TechSocial/TechSocial/TechSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechSocialApp.swift
+//  TechSocial
+//
+//  Created by Robert Fusting on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct TechSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
