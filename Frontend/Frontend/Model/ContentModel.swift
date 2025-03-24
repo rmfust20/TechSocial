19c9654df99ba2e73507d096dae6b227fa73585b
rmfust20 <robertmfusting@gmail.com>
2025-03-24 15:18:44 -0400

    Frontend data model implementation

---
+++ Frontend/Frontend/Model/ContentModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContentModel.swift
+//  Frontend
+//
+//  Created by Robert Fusting on 3/24/25.
+//
+
+import Foundation
---
+++ Frontend/Frontend/Model/ProjectModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProjectModel.swift
+//  Frontend
+//
+//  Created by Robert Fusting on 3/24/25.
+//
+
+import Foundation
