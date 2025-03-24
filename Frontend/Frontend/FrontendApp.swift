2bf9cbaa3781b583633ce4770e90bcf050736078
rmfust20 <robertmfusting@gmail.com>
2025-03-24 12:05:02 -0400

    frontend init

---
+++ Frontend/Frontend/FrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrontendApp.swift
+//  Frontend
+//
+//  Created by Robert Fusting on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct FrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
